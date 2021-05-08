7ff74a4a7bd7b72c29aff7e11d68fb170f0767f3
Shawn Han <shawn@Shawnui-MacBookPro.local>
2021-05-08 12:56:26 +0900

    [기능][auth] naver

--- src/infra/passport/passport.ts
+++ src/infra/passport/passport.ts
@@ -18,6 +18,76 @@ passport.serializeUser((user, done) => { done(null, user); });
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
+passport.use('naver', new KakaoStrategy({
+  clientID: '6MAo7EODEA9uoCSypc9T',
+  clientSecret: 'a0VCOhPQRj',
+  callbackURL: 'http://everyweardev-env.eba-azpdvh2m.ap-northeast-2.elasticbeanstalk.com/api/v1/auth/naver/callback',
+}, async (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  const conn = QueryExecutor.getInstance().getReadConnection();
+  let [user] = await conn('everywear_user as User')
+    .select('User.*', 'Skin.skinType', 'Body.bodyType', 'Face.faceType', 't1.apple')
+    .where({ provider: profile.provider, oAuthId: profile.id })
+    .leftJoin('everywear_skinType as Skin', function () {
+      this.on('User.skinTypeId', '=', 'Skin.skinTypeId');
+    })
+    .leftJoin('everywear_faceType as Face', function () {
+      this.on('User.faceTypeId', '=', 'Face.faceTypeId');
+    })
+    .leftJoin('everywear_bodyType as Body', function () {
+      this.on('User.bodyTypeId', '=', 'Body.bodyTypeId');
+    })
+    .leftJoin(conn.raw(`
+      (SELECT 
+        userId, SUM(value) AS apple 
+      FROM 
+        everywear_apple 
+      GROUP BY userId) AS t1 
+      ON 
+        User.userId = t1.userId
+      `));
+  if (!user) {
+    const [id] = await conn('everywear_user').insert({ provider: profile.provider, oAuthId: profile.id });
+    const newUser = new User(id,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      Number(profile.id),
+      profile.provider as AuthProvider,
+      null);
+    return done(null, {
+      ...newUser,
+      token: JwtTokenGenerator.get({
+        userId: newUser.id,
+        name: newUser.name,
+        faceType: newUser.faceType,
+        skinType: newUser.skinType,
+        bodyType: newUser.bodyType,
+        mail: newUser.mail,
+        apple: newUser.apple,
+      }),
+    });
+  }
+  user = UserMapper.toService(user);
+  return done(null, {
+    ...user,
+    token: JwtTokenGenerator.get({
+      userId: user.id,
+      name: user.name,
+      faceType: user.faceType,
+      skinType: user.skinType,
+      bodyType: user.bodyType,
+      mail: user.mail,
+      apple: user.apple,
+    }),
+  });
+}));
 
 passport.use('kakao', new KakaoStrategy({
   clientID: 'de2237bf43e6582ed18ac447ac76527b',
